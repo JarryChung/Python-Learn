7c279f5a6376d4f113357215869b671055a01142
JarryChung <jarrychung@outlook.com>
2018-01-26 19:31:10 +0800

    I/O以及格式化输出

--- python_1.py
+++ python_1.py
@@ -1,1 +1,9 @@
-print("hello world")
+# -*- coding: utf-8 -*-
+
+name = input("Enter you name:")
+print("hello",name,".")
+
+height = input("Enter your height:")
+weight = input("Enter your weight:")
+print("Height: %s, Weight: %s" % (height,weight))
+print("Number: %d" % 5)
