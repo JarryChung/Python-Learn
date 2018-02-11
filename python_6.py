b4535ca6e4c59de4dd462ba75e4268d3908634db
JarryChung <jarrychung@outlook.com>
2018-02-11 22:21:41 +0800

    新进度：初识高阶函数

--- python_6.py
+++ python_6.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
 
+from functools import reduce
+
+
 # 高阶函数
 func = abs
 print(func(-9))
 
 
-def abs_add(x, y, f):
-    return f(x) + f(y)
+def abs_add(x, f):
+    return f(x) + f(x)
 
 
-print(abs_add(-5, 5, func))
+print(abs_add(-5, func))
+
+
+def inc(x):
+    return x+1
+
+
+def add(v, u):
+    return v + u
+
+
+L = list(map(inc, [1, 2, 3]))
+print(L)
+
+LL = reduce(add, [1, 2, 3, 4, 5])
+print(LL)
 
