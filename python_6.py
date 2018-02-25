293a2d9e085b3659483ff726f16ab1c2ac2f378a
JarryChung <jarrychung@outlook.com>
2018-02-25 16:22:40 +0800

    新进度：高阶函数filter

--- python_6.py
+++ python_6.py
@@ -29,3 +29,16 @@ print(L)
 LL = reduce(add, [1, 2, 3, 4, 5])
 print(LL)
 
+
+# reduce & map 综合使用，将字符串转化为数字
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+
+    def char2num(s):
+        return DIGITS[s]
+    return reduce(fn, map(char2num, s))
+
